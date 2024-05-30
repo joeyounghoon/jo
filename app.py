92b7c195a791fc832dd9ec37ecbfb9275047a569
joeyounghoon <yhjoe36511@gmail.com>
2024-05-30 16:01:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
-import openai
-import streamlit as st 
+import streamlit as st
+from openai import OpenAI
 
-st.write("talk with your gpt")
-# 메모리 초기화
-if "messages" not in st.session_state: 
- st.session_state.messages = [] 
-# 저장한 메시지 사용자/응답 구분해서 보여주기
-for msg in st.session_state.messages: 
- with st.chat_message(msg["role"]): 
- st.markdown(msg["content"])
-# 사용자 입력과 LLM 응답
-if prompt := st.chat_input("What is up?"): 
- # 사용자 메시지 보여주기
- st.chat_message("user").markdown(prompt) 
- # 메모리에 사용자 메시지 저장
- st.session_state.messages.append({"role": "user", "content": prompt}) 
- # Assistant API Thread의 마지막 Message 가져오는 기능 추가 필요
- response = f"Echo: {prompt}" 
- # LLM 응답 보여주기
- with st.chat_message("assistant"): 
- st.markdown(response) 
- # 메모리에 LLM 응답 저장
- st.session_state.messages.append({"role": "assistant", "content": response})
+@st.cache_data
+def generate_image(image_prompt):
+ client=OpenAI(api_key=st.session_state['OPENAI_API_KET'])
+ response = client.images.generate(
+  model="dall-e-3",
+  precpt=image_prompt,
+ )
+ image_url=response.data[0].url
+ return image_url
+st.session_state['image_prompt'] = st.text_input("어떤 그림을 원하시나요?",value=st.session_state.get("image_prompt",''))
+
+if st.button("Run"):
+ with st.spinner("Generating..."):
+  image_url = generate_image(st.session_state['image_prompt'])
+  st.markdown(f"{({st.session_state['image_prompt']}){(image_url)}}")
